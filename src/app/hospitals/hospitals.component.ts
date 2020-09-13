dcf00613c7f09524bd95f1d34f0ff38b8fa598a6
pradeep <rekkapallipradeepG@gmail.com>
2020-09-13 16:49:05 +0530

    hospital page done responsive is there

--- src/app/hospitals/hospitals.component.ts
+++ src/app/hospitals/hospitals.component.ts
@@ -11,5 +11,46 @@ export class HospitalsComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+ 
+  cartarr=[{
+    "state": "A & N Islands",
+    "name": "Andaman & Nicobar Islands Insitute of Medical Sciences, Port Blair",
+    "city": "Port Blair",
+    "ownership": "Govt.",
+    "admissionCapacity": 100,
+    "hospitalBeds": 460
+  },
+  {
+    "state": "Andhra Pradesh",
+    "name": "ACSR Government Medical College Nellore",
+    "city": "Nellore",
+    "ownership": "Govt.",
+    "admissionCapacity": 150,
+    "hospitalBeds": 750
+  },
+  {
+    "state": "Andhra Pradesh",
+    "name": "Alluri Sitaram Raju Academy of Medical Sciences, Eluru",
+    "city": "Eluru",
+    "ownership": "Trust",
+    "admissionCapacity": 150,
+    "hospitalBeds": 1070
+  },
+  {
+    "state": "Andhra Pradesh",
+    "name": "Andhra Medical College, Visakhapatnam",
+    "city": "Visakhapatnam",
+    "ownership": "Govt.",
+    "admissionCapacity": 200,
+    "hospitalBeds": 2017
+  },
+  {
+    "state": "Andhra Pradesh",
+    "name": "Apollo Institute of Medical Sciences and Research, Chittoor",
+    "city": "Chittoor",
+    "ownership": "Society",
+    "admissionCapacity": 150,
+    "hospitalBeds": 0
+  }]
+  total=[]
 }
